86592cde8a38e675730fc4d2c358e3260b271394
Stephen Belisario <stephenbelisario@Stephens-MBP.attlocal.net>
2023-08-25 13:44:48 -0700

    adding more pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,8 +8,14 @@ import MobileNav from './MobileNav';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return <header className='bg-pink-200 fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center
-  '>Header</header>
+  return <header className='bg-pink-200 fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center'>
+    <div className='flex flex-col lg:flex-row lg:items-center w-full justify-between'>
+        {/* logo */}
+        <Link to={'/'}>
+          <img src={Logo} alt='logo'/>
+        </Link>
+    </div>
+    </header>
 };
 
 export default Header; 
