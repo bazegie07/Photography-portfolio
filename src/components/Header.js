7057ff965fe7570f85185fd216ec1a44b287cc50
Stephen Belisario <stephenbelisario@Stephens-MBP.attlocal.net>
2023-08-28 13:12:45 -0700

    adding function on the nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,10 +11,37 @@ const Header = () => {
   return <header className='bg-pink-200 fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center'>
     <div className='flex flex-col lg:flex-row lg:items-center w-full justify-between'>
         {/* logo */}
-        <Link to={'/'}>
+        <Link to={'/' } className='max-w-[200px]'>
           <img src={Logo} alt='logo'/>
         </Link>
+        {/* nav- initially hidden - show on desktop mode */}
+        <nav className='hidden lg:flex gap-x-12 font-semibold'>
+          <Link 
+          to={'/'}
+          className='text-[#696c6d] hover:text-primary transition'
+          >Home
+          </Link>
+          <Link 
+          to={'/about'}
+          className='text-[#696c6d] hover:text-primary transition'
+          >About
+          </Link>
+          <Link 
+          to={'/portfolio'}
+          className='text-[#696c6d] hover:text-primary transition'
+          >Portfolio
+          </Link>
+          <Link 
+          to={'/contact'}
+          className='text-[#696c6d] hover:text-primary transition'
+          >Contact
+          </Link>
+        </nav>
     </div>
+      {/* social */}
+      <Socials/>
+      {/* mobile nav */}
+      <MobileNav/>
     </header>
 };
 
